917a527ba5b7222b3e68899b2653537ffc2cc481
vadim <yurkinvadim99@gmail.com>
2025-08-27 15:33:41 +0200

    Change upload_to behavior

--- photos/models.py
+++ photos/models.py
@@ -1,15 +1,17 @@
 from django.db import models
+from django.utils.timezone import now
 
 from cloud_gallery.settings import AUTH_USER_MODEL
 
 
-def user_directory_path(instance, filename):
-    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
-    return "user_{0}/{1}".format(instance.user.id, filename)
+def upload_to_user_directory_with_timestamp(instance, filename):
+    ext = filename.split('.')[-1]
+    return f'user_{instance.user.id}/{now().strftime("%Y%m%d%H%M%S")}.{ext}'
+
 
 class Photo(models.Model):
     user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='photos')
-    image = models.ImageField(upload_to=user_directory_path)
+    image = models.ImageField(upload_to=upload_to_user_directory_with_timestamp)
     title = models.CharField(max_length=100, blank=False, null=False)
     description = models.TextField(blank=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
